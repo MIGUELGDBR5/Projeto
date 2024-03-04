3e1b5bf00efcd61811b17019c1a26514b969d8ed
Miguel Coelho <coelhomiguel485@gmail.com>
2024-03-03 22:34:55 -0300

    MIGUELGDBR5 profile edit

--- Projeto/script.js
+++ Projeto/script.js
@@ -9,11 +9,11 @@ function toggleMode() {
     // substituir a imagem 
     // se tiver light mode adicionar light
     if(html.classList.contains('light')) {
-      img.setAttribute('src', './assets/avatar-light.png')
+      img.setAttribute('src', 'https://pbs.twimg.com/profile_images/1556761716041605120/SRNCDSHV_400x400.jpg')
     
       img.setAttribute('alt', '"Foto de Mayk Brito sorrindo, usando óculos escuros, de jaqueta preta, e fundo colorido com roxo e azul."')
     } else {
-        img.setAttribute('src', './assets/avatar.png')
+        img.setAttribute('src', 'https://steamuserimages-a.akamaihd.net/ugc/1814365766212573958/04B7867DCE7F7DC885EF9D2B31FCDC9EF2D03443/?imw=512&&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=false')
 
         img.setAttribute('alt', '"Foto de Mayk Brito sorrindo, usando óculos, de camisa preta, barba e fundo amarelo."')
     }
